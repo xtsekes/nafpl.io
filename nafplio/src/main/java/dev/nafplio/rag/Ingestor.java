2fb7a23aa362dea31b311bef48a8ab9e03fae3df
Arnold Dziudzik <arndzk@Arnold-MacBook-Pro.local>
2024-11-05 17:23:16 +0200

    resolve merge conflicts;

---
+++ nafplio/src/main/java/dev/nafplio/rag/Ingestor.java
@@ -0,0 +1,38 @@
+package dev.nafplio.rag;
+
+import dev.langchain4j.data.document.Document;
+import dev.langchain4j.data.document.loader.FileSystemDocumentLoader;
+import dev.langchain4j.model.embedding.EmbeddingModel;
+import dev.langchain4j.store.embedding.EmbeddingStoreIngestor;
+import dev.langchain4j.store.embedding.pgvector.PgVectorEmbeddingStore;
+import io.quarkus.logging.Log;
+import io.quarkus.runtime.Startup;
+import jakarta.inject.Singleton;
+
+import java.nio.file.Path;
+import java.util.List;
+
+import static dev.langchain4j.data.document.splitter.DocumentSplitters.recursive;
+
+@Singleton
+@Startup
+public class Ingestor {
+
+    public Ingestor(PgVectorEmbeddingStore store, EmbeddingModel embedding) {
+
+        EmbeddingStoreIngestor ingestor = EmbeddingStoreIngestor.builder()
+                .embeddingStore(store)
+                .embeddingModel(embedding)
+                .documentSplitter(recursive(1024, 0))
+                .build();
+
+        Path dir = Path.of("documents");
+        List<Document> documents = FileSystemDocumentLoader.loadDocuments(dir);
+        Log.info("Ingesting " + documents.size() + " documents");
+
+        ingestor.ingest(documents);
+
+        Log.info("Document ingested");
+    }
+
+}
