e9b2c67ba43cc6e98059e2bf11dfd74481eb3efd
Panagiotis Konstantellos <ntellos13@hotmail.com>
2024-11-06 12:00:44 +0200

    added RAG

--- nafplio/src/main/java/dev/nafplio/service/AiService.java
+++ nafplio/src/main/java/dev/nafplio/service/AiService.java
@@ -1,10 +1,10 @@
 package dev.nafplio.service;
 
+import dev.nafplio.rag.Retriever;
 import io.quarkiverse.langchain4j.RegisterAiService;
 import io.smallrye.mutiny.Multi;
 
-@RegisterAiService
+@RegisterAiService(retrievalAugmentor = Retriever.class)
 public interface AiService {
-
     Multi<String> chat(String prompt);
 }
